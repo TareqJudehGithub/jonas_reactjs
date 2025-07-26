2aaedd78932458b8e5a07dae9071732682696656
TareqJudehGithub <tareq.joudeh@gmail.com>
2025-07-26 20:58:48 +0300

    Upgraded project: date-counter with input:range element, removing the old + - buttons rresponsible for update the step state. Added an input:text element for count state, where users now can change the count state by using that input field directly, or using the range input element.

--- Practice/date-counter/src/App.js
+++ Practice/date-counter/src/App.js
@@ -5,33 +5,23 @@ function App() {
 // components
 function Counter() {
 	const [count, setCount] = useState(0);
-	const [step, setStep] = useState(1);
+	const [range, setRange] = useState(1);
 
 	const dateToday = new Date();
 	dateToday.setDate(dateToday.getDate() + count);
 	// Steps
-	function handleStepMinus() {
-		if (step > 1) {
-			return setStep((s) => s - 1);
-		}
-		// }
-	}
-	function handleStepPlus() {
-		if (step < 30 && count < 30) {
-			return setStep((s) => s + 1);
-		}
-	}
-	function handleStepReset() {
-		return setStep((s) => (s = 1));
+
+	function handleStepReset(e) {
+		setRange((s) => (s = 1));
 	}
 
 	// Counts
 	function handleCountMinus() {
-		return setCount((c) => c - step);
+		return setCount((c) => c - range);
 	}
 	function handleCountPlus() {
 		if (count < 365) {
-			return setCount((c) => c + step);
+			return setCount((c) => c + range);
 		}
 	}
 	function handleCountReset() {
@@ -40,17 +30,28 @@ function Counter() {
 
 	return (
 		<div className="container">
+			{/* Steps UI */}
 			<p>
-				<button onClick={handleStepMinus}>-</button>
-				<span>Step: {step} </span>
-				<button onClick={handleStepPlus}>+</button>
+				<input
+					type="range"
+					min="0"
+					max="10"
+					value={range}
+					onChange={(e) => setRange(Number(e.target.value))}
+				/>
+				<span>Step(s): {range} </span>
 				<button className="reset" onClick={handleStepReset}>
 					Reset
 				</button>
 			</p>
+
 			<p>
 				<button onClick={handleCountMinus}>-</button>
-				<span>Count: {count}</span>
+				<input
+					type="text"
+					value={count}
+					onChange={(e) => setCount(Number(e.target.value))}
+				/>
 				<button onClick={handleCountPlus}>+</button>
 
 				<button className="reset" onClick={handleCountReset}>
@@ -65,3 +66,34 @@ function Counter() {
 }
 
 export default App;
+
+/*  
+buttons solution
+
+const [step, setStep] = useState(0);
+
+function handleStepMinus() {
+		if (step > 1) {
+			return setStep((s) => s - 1);
+		}
+		// }
+	}
+	function handleStepPlus() {
+		if (step < 30 && count < 30) {
+			return setStep((s) => s + 1);
+		}
+	}
+	function handleStepReset(e) {
+		//setStep((s) => (s = 1));
+		setRange((s) => (s = 1));
+	}
+
+<button onClick={handleStepMinus}>-</button> 
+<span>Step(s): {step} </span>
+<button onClick={handleStepPlus}>+</button> 
+
+<button onClick={ handleCountMinus }>-</button>				
+				<span>Count: {count}</span>
+				<button onClick={handleCountPlus}>+</button>
+
+*/
