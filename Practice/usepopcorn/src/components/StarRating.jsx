dda80d69353a8a5fe566c03b6ffe875170c1e8ab
TareqJudehGithub <tareq.joudeh@gmail.com>
2025-08-14 13:39:23 +0300

    Finished implementing the Star component, adding needed functionality where users can set rating 1-5 using star icons.

--- Practice/usepopcorn/src/components/StarRating.jsx
+++ Practice/usepopcorn/src/components/StarRating.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import Star from "./Star";
 
+import PropType from "prop-types";
+
 const containerStyle = {
 	display: "flex",
-	alignItems: "center",
+	alignItems: "start",
 	gap: "16px",
 };
 
@@ -11,28 +13,50 @@ const starContainerStyle = {
 	display: "flex",
 };
 
-const textStyle = {
-	lineHeight: "1",
-	margin: "0",
+StarRating.propTypes = {
+	maxRating: PropType.number,
+	defaultRating: PropType.number,
+	color: PropType.string,
+	size: PropType.number,
+	messages: PropType.array,
+	className: PropType.string,
+	onSetRating: PropType.func,
 };
 
-function StarRating({ maxRating = 5 }) {
+function StarRating({
+	maxRating = 5,
+	color = "#fcc419",
+	size = 36,
+	className,
+	messages = [],
+	defaultRating = 0,
+	onSetRating,
+}) {
 	//  prop = 5 is the default prop value.
 
 	// States
-	const [rating, setRating] = useState(0);
+	const [rating, setRating] = useState(defaultRating);
 	const [tempRating, setTempRating] = useState(0);
 
 	// Handlers
 	function handleRating(rating) {
 		/* This handle updates the rating state by clicking on the star index. We
-		add 1 because arrays are 0 index, while rating starts with 1 */
+		add 1 because arrays are 0 indexing, while rating starts with 1 */
 		setRating(rating);
 		console.log(`Rating is set to ${rating}`);
+
+		onSetRating && onSetRating(rating); // passing set handler to another component
 	}
+	// Styles
+	const textStyle = {
+		lineHeight: "1",
+		margin: "0",
+		color,
+		fontSize: `${size}px`,
+	};
 
 	return (
-		<div style={containerStyle}>
+		<div style={containerStyle} className={className}>
 			<div style={starContainerStyle}>
 				{Array.from({ length: maxRating }, (value, index) => (
 					<Star
@@ -43,12 +67,22 @@ function StarRating({ maxRating = 5 }) {
 						// Check if a star is full or empty. If true, the full
 						// star will be rendered. False? then empty star will be
 						// rendered instead.
-
+						color={color}
+						size={size}
 						full={tempRating ? tempRating >= index + 1 : rating >= index + 1}
 					/>
 				))}
 			</div>
-			<p style={textStyle}>{tempRating || rating || ""}</p>
+			<p style={textStyle}>
+				{messages.length === maxRating // Check if the array length is equal to maxRating length.
+					? messages[
+							tempRating
+								? tempRating - 1 // Switch back to array indexing for messages array.
+								: rating - 1
+					  ] // after checking if tempRating or rating is not null.
+					: tempRating || rating || ""}
+				{/* {tempRating || rating || ""}  */}
+			</p>
 		</div>
 	);
 }
--- Practice/usepopcorn/src/index.js
+++ Practice/usepopcorn/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 //import App from "./App";
 import StarRating from "./components/StarRating";
-
+import Test from "./components/Test";
 // Styling
 import "./index.css";
 
@@ -11,6 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		{/* <App /> */}
-		<StarRating maxRating={5} />
+		<StarRating
+			maxRating={5}
+			className="test2"
+			messages={["Terrible", "Bad", "Okay", "Good", "Amazing!"]}
+		/>
+		<StarRating color="red" className="test" size={20} defaultRating={3} />
+		<Test />
 	</React.StrictMode>
 );
