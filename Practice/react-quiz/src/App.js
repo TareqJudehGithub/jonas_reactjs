55482b813564164606a532cd8b2142a962aae525
TareqJudehGithub <tareq.joudeh@gmail.com>
2025-09-04 23:42:19 +0300

    Implemented useReducer hook to the-react-quiz project. Adding two states already: ready and error. The ready state is when the data is sucessfully fetched, while the error is of course in case it was not.

--- Practice/react-quiz/src/App.js
+++ Practice/react-quiz/src/App.js
@@ -1,9 +1,63 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
+	// States
+	const initialState = {
+		questions: [],
+
+		// The states(status) the application can be in: "loading", "error", "ready", "active", "finish"
+		status: "loading", // status will tell the application where exactly we are at.
+	};
+
+	function reducer(state, action) {
+		switch (action.type) {
+			case "dataReceived":
+				return {
+					...state,
+					question: action.payload,
+					status: "ready",
+				};
+			case "dataFailed":
+				return {
+					...state,
+					status: "error",
+				};
+
+			default:
+				throw new Error("Action unknown");
+		}
+	}
+
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	useEffect(function () {
+		const url = "http://localhost:8000/questions";
+
+		async function getQuestions() {
+			try {
+				const response = await fetch(url);
+				const data = await response.json();
+
+				// dataReceived is the data we just received right after fetch
+				dispatch({ type: "dataReceived", payload: data });
+			} catch (err) {
+				dispatch({
+					type: "dataFailed",
+				});
+			}
+		}
+		getQuestions();
+	}, []);
+
 	return (
 		<div className="app">
-			<DateCounter />
+			<Header />
+			<Main>
+				<p>1/15</p>
+				<p>Question?</p>
+			</Main>
 		</div>
 	);
 }
